25fe45722125aaeb853a2b4aa07ec081fb947886
csivakolundu <csivakolundu@paypal.com>
2015-10-03 19:56:54 -0700

    1st draft for schema.

--- server/dao/src/main/resources/META-INF/db/schema.sql
+++ server/dao/src/main/resources/META-INF/db/schema.sql
@@ -5,3 +5,27 @@ CREATE TABLE `app_config` (
   `value` varchar(4096) NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+
+CREATE TABLE business (
+  id INT ,
+  category varchar(20),
+  latitude double,
+  longitude double,
+  addressId int,
+  businessName varchar(20),
+  businessDescription varchar(20),
+  likes int,
+  PRIMARY KEY (userId)
+  );
+  
+  
+
+  CREATE TABLE address (
+  addressId int,
+  street varchar(50),
+  city varchar(20),
+  state varchar (20),
+  zipCode smallint,
+  PRIMARY KEY (addressId)
+  );  
