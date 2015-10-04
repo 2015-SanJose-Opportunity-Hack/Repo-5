eb5a0e158a974a0fe32cf8ef6da5aaeb9b53fc57
csivakolundu <csivakolundu@paypal.com>
2015-10-04 11:27:37 -0700

    prototype done.

--- server/resources/assets/js/hackathon.js
+++ server/resources/assets/js/hackathon.js
@@ -50,11 +50,22 @@
 		}
 
 		
-		function openModel(src) {
-			$('#myModal').on('show', function () {
 		
-		        $('iframe').attr("src",src);
-		      
+		
+		( function( $ ) {
+			$( document ).ready(function() {
+			// Cache the elements we'll need
+			var menu = $('#cssmenu');
+			var menuList = menu.find('ul:first');
+			var listItems = menu.find('li').not('#responsive-tab');
+
+			// Create responsive trigger
+			menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
+
+			// Toggle menu visibility
+			menu.on('click', '#responsive-tab', function(){
+				listItems.slideToggle('fast');
+				listItems.addClass('collapsed');
 			});
-		    $('#myModal').modal({show:true})
-		}	
+			});
+			} )( jQuery );
